48b2b439b479601b665b7d62835c977bfcd47d56
Lauren-Almeida <128049276+Lauren-Almeida@users.noreply.github.com>
2023-07-05 16:19:34 -0300

    Criação da CatalogoAPI

---
+++ CatalogoAPI/Context/AppDbContext.cs
@@ -0,0 +1,51 @@
+using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Produto>? Produtos { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome)
+                                  .HasMaxLength(100)
+                                  .IsRequired();
+
+            mb.Entity<Categoria>().Property(c => c.Descricao)
+                                  .HasMaxLength(150)
+                                  .IsRequired();
+
+            mb.Entity<Produto>().HasKey(p => p.produtoId);
+
+            mb.Entity<Produto>().Property(p => p.Nome)
+                                  .HasMaxLength(100)
+                                  .IsRequired();
+
+            mb.Entity<Produto>().Property(p => p.Descricao).HasMaxLength(150);
+
+            mb.Entity<Produto>().Property(p => p.Imagem).HasMaxLength(100);
+
+            mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+
+            //relacionamento
+
+            mb.Entity<Produto>()
+                .HasOne<Categoria>(c => c.Categoria)
+                .WithMany(p => p.Produtos)
+                    .HasForeignKey(c => c.CategoriaId);
+
+
+
+
+        }
+
+    }
+}
